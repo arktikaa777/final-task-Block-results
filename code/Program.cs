8fb14c6d2cf31a1eb0bd714f24fe65f750f9cb29
Genya <arktikaa777@gmail.com>
2022-10-19 22:22:05 +1300

    Add Code

---
+++ code/Program.cs
@@ -0,0 +1,73 @@
+﻿// Написать программу, решающую поставленную задачу
+string[] array = new string[] {};
+
+string Userenter = ReadInput("Input the command: ");
+switch (Userenter)
+{
+    case "1":
+        array = new string[] { "Hello", "2", "world", ":-)" };
+        break;
+    case "2":
+        array = new string[] { "1234", "1567", "-2", "computer science" };
+        break;
+    case "3":
+        array = new string[] { "Russia", "Denmark", "Kazan" };
+        break;
+    default:
+        Console.WriteLine($"{Userenter} - Wrong command");
+        break;
+}
+
+
+int LengthNewArray  = 0;
+for (int i = 0; i <= array.Length - 1; i++)
+{
+    if (array[i].Length <= 3) LengthNewArray
+    ++;
+}
+
+string[] newArray = new string[LengthNewArray];
+int x = 0;
+
+for (int i = 0; i <= array.Length - 1; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        newArray[x] = array[i];
+        x++;
+    }
+}
+
+PrintArray(array);
+Console.Write("→ ");
+PrintArray(newArray);
+
+// Функция: Вывод команд для работы с программой
+void Commands()
+{
+    Console.WriteLine();
+    Console.WriteLine("СПИСОК КОМАНД:");
+    Console.WriteLine("1 – использовать массив: [“Hello”, “2”, “world”, “:-)”]");
+    Console.WriteLine("2 – использовать массив: [“1234”, “1567”, “-2”, “computer science”]");
+    Console.WriteLine("3 – использовать массив: [“Russia”, “Denmark”, “Kazan”]");
+    Console.WriteLine();
+}
+
+// Функция ввода
+string ReadInput(string msg)
+{
+    Console.Write(msg);
+    return Console.ReadLine();
+}
+
+//  Функция вывода массива в терминал
+void PrintArray(string[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"“{array[i]}”, ");
+    }
+    Console.Write("] ");
+}
+
